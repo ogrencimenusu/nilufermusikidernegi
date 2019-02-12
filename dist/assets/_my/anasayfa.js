ede3bac714b41dba941d7b72f29a67bf26fdca85
ogrencimenusu <srht.akyol.1649@gmail.com>
2019-02-12 16:51:36 +0300

    İlk dosyalar

---
+++ dist/assets/_my/anasayfa.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    $('.popup-gallery').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
+
+            tPrev: 'Previous', // title for left button
+            tNext: 'Next ', // title for right button
+
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+            titleSrc: function(item) {
+                return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+            }
+        },
+        callbacks: {
+
+            buildControls: function() {
+                // re-appends controls inside the main container
+                this.contentContainer.append(this.arrowLeft.add(this.arrowRight));
+            }
+        }
+    });
+});
---
+++ gulpfile.js
@@ -0,0 +1,72 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const minifyCSS = require('gulp-csso');
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const del = require('del');
+const runSequence = require('run-sequence');
+
+gulp.task('browser-sync', () => {
+    browserSync.init({
+        server: {
+            baseDir: "dist"
+        }
+    });
+});
+gulp.task('cssMy', () => {
+    return gulp.src('src/scss/**/*.scss')
+        .pipe(sass({
+            outputStyle: 'nested',
+            precision: 10,
+            includePaths: ['.']
+        }).on('error', sass.logError))
+        .pipe(minifyCSS())
+        .pipe(autoprefixer())
+        .pipe(concat('app.min.css'))
+        .pipe(gulp.dest('dist/assets/_my/'))
+        .pipe(browserSync.stream());
+});
+gulp.task('anasayfaJs', () => {
+    return gulp.src('src/js/anasayfa.js')
+        .pipe(concat('anasayfa.js'))
+        .pipe(gulp.dest('dist/assets/_my/'))
+        .pipe(browserSync.stream());
+});
+gulp.task('mainJs', () => {
+    return gulp.src('src/js/main.js')
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('dist/assets/_my/'))
+        .pipe(browserSync.stream());
+});
+gulp.task('html', () => {
+    gulp.src('src/**/*.html')
+        .pipe(gulp.dest('dist'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('delete', () => del([
+    'dist/assets/_my/**/*.css',
+    'dist/assets/_my/anasayfa.js',
+    'dist/assets/_my/main.js',
+    'dist/**/*.html'
+]));
+
+gulp.task('watch', () => {
+    gulp.watch("src/scss/**/*.scss", ['cssMy']);
+    gulp.watch("src/js/anasayfa.js", ['anasayfaJs']);
+    gulp.watch("src/js/main.js", ['mainJs']);
+    gulp.watch("src/**/*.html", ['html']);
+});
+
+gulp.task('default', () => {
+    runSequence(
+        'delete',
+        'html',
+        'cssMy',
+        'anasayfaJs',
+        'mainJs',
+        'browser-sync',
+        'watch'
+    );
+});
